f3acc164d467be8ca65e3676d10574779afe8892
Fabian Schuiki <fabian@schuiki.ch>
2025-08-01 18:14:36 -0700

    Add more LLVM namespace imports

--- include/silicon/LLVM.h
+++ include/silicon/LLVM.h
@@ -8,11 +8,15 @@
 
 #pragma once
 
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/ADT/DenseSet.h"
 #include "llvm/ADT/PointerUnion.h"
 #include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/ADT/TypeSwitch.h"
 #include "llvm/Support/Casting.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/LogicalResult.h"
 #include "llvm/Support/SMLoc.h"
 #include "llvm/Support/SourceMgr.h"
@@ -21,6 +25,9 @@ namespace silicon {
 using llvm::APInt;
 using llvm::ArrayRef;
 using llvm::cast;
+using llvm::dbgs;
+using llvm::DenseMap;
+using llvm::DenseSet;
 using llvm::dyn_cast;
 using llvm::dyn_cast_or_null;
 using llvm::failed;
@@ -31,6 +38,8 @@ using llvm::isa_and_nonnull;
 using llvm::LogicalResult;
 using llvm::ParseResult;
 using llvm::PointerUnion;
+using llvm::SmallDenseMap;
+using llvm::SmallDenseSet;
 using llvm::SmallString;
 using llvm::SmallVector;
 using llvm::SMLoc;
@@ -39,4 +48,5 @@ using llvm::StringRef;
 using llvm::succeeded;
 using llvm::success;
 using llvm::Twine;
+using llvm::TypeSwitch;
 } // namespace silicon
