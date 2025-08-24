6c497b0327280c8c08616d192d3ab690431431d1
Fabian Schuiki <fabian@schuiki.ch>
2025-08-24 15:37:50 -0700

    [MIR] Make ConstantOp assign a sensible name to its result

--- lib/MIR/Ops.cpp
+++ lib/MIR/Ops.cpp
@@ -18,8 +18,40 @@ using namespace mir;
 #define GET_OP_CLASSES
 #include "silicon/MIR/Ops.cpp.inc"
 
+/// Print an abbreviation of a type that can be used in an assembly name.
+static bool getTypeAbbrev(llvm::raw_ostream &os, Type type) {
+  return TypeSwitch<Type, bool>(type)
+      .Case<IntType>([&](auto type) {
+        os << type.getMnemonic();
+        return true;
+      })
+      .Default([](auto) { return false; });
+}
+
+/// Print an abbreviation of an attribute that can be used in an assembly name.
+static bool getAttrAbbrev(llvm::raw_ostream &os, Attribute attr) {
+  return TypeSwitch<Attribute, bool>(attr)
+      .Case<IntAttr>([&](auto attr) {
+        os << 'c' << attr.getValue();
+        return true;
+      })
+      .Default([](auto) { return false; });
+}
+
 //===----------------------------------------------------------------------===//
 // ConstantOp
 //===----------------------------------------------------------------------===//
 
 OpFoldResult ConstantOp::fold(FoldAdaptor adaptor) { return getValue(); }
+
+void ConstantOp::getAsmResultNames(
+    llvm::function_ref<void(Value, StringRef)> setNameFn) {
+  SmallString<32> buffer;
+  llvm::raw_svector_ostream bufferStream(buffer);
+  if (!getAttrAbbrev(bufferStream, getValue()))
+    return;
+  bufferStream << '_';
+  if (!getTypeAbbrev(bufferStream, getType()))
+    buffer.pop_back();
+  setNameFn(getResult(), buffer);
+}
