6a74fc075c8983d4fda61341ee4017ffc2139d68
Fabian Schuiki <fabian@schuiki.ch>
2025-08-24 14:52:52 -0700

    Adjust license headers

--- tools/silicon-opt/silicon-opt.cpp
+++ tools/silicon-opt/silicon-opt.cpp
@@ -1,13 +1,13 @@
 //===-silicon-opt.cpp -----------------------------------------------------===//
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
+// Part of Silicon, licensed under the Apache License v2.0 with LLVM Exceptions.
+// See the LICENSE file for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "silicon/RegisterAll.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
