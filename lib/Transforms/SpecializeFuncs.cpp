59841840cef7064ed58c52dd572cadf26ab9961a
Fabian Schuiki <fabian@schuiki.ch>
2025-09-02 20:28:38 -0700

    [Passes] Add simple function specialization pass

---
+++ lib/Transforms/SpecializeFuncs.cpp
@@ -0,0 +1,134 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of Silicon, licensed under the Apache License v2.0 with LLVM Exceptions.
+// See the LICENSE file for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "silicon/HIR/Ops.h"
+#include "silicon/MIR/Attributes.h"
+#include "silicon/MIR/Ops.h"
+#include "silicon/Support/MLIR.h"
+#include "silicon/Transforms/Passes.h"
+#include "mlir/IR/IRMapping.h"
+#include "llvm/Support/Debug.h"
+
+using namespace mlir;
+using namespace silicon;
+
+#define DEBUG_TYPE "specialize-funcs"
+
+namespace silicon {
+#define GEN_PASS_DEF_SPECIALIZEFUNCSPASS
+#include "silicon/Transforms/Passes.h.inc"
+} // namespace silicon
+
+namespace {
+struct SpecializeFuncsPass
+    : public silicon::impl::SpecializeFuncsPassBase<SpecializeFuncsPass> {
+  void runOnOperation() override;
+  hir::FuncOp specialize(hir::FuncOp originalFunc,
+                         mir::SpecializedFuncAttr spec);
+};
+} // namespace
+
+void SpecializeFuncsPass::runOnOperation() {
+  auto &symbolTable = getAnalysis<SymbolTable>();
+  DenseMap<mir::SpecializedFuncAttr, hir::FuncOp> funcs;
+  SmallVector<Operation *> worklist;
+  worklist.push_back(getOperation());
+  while (!worklist.empty()) {
+    auto *op = worklist.pop_back_val();
+    auto result = op->walk([&](mir::ConstantOp op) {
+      auto attr = dyn_cast<mir::SpecializedFuncAttr>(op.getValue());
+      if (!attr)
+        return WalkResult::advance();
+      auto &func = funcs[attr];
+      if (!func) {
+        LLVM_DEBUG(llvm::dbgs() << "Specializing " << attr << "\n");
+        func = specialize(
+            symbolTable.lookup<hir::FuncOp>(attr.getFunc().getAttr()), attr);
+        if (!func)
+          return WalkResult::interrupt();
+        symbolTable.insert(func);
+        worklist.push_back(func);
+      }
+      OpBuilder builder(op);
+      auto newOp = hir::ConstantFuncOp::create(builder, op.getLoc(),
+                                               func.getSymNameAttr());
+      LLVM_DEBUG(llvm::dbgs()
+                 << "Replacing " << op << " with " << newOp << "\n");
+      op.getResult().replaceAllUsesWith(newOp);
+      op.erase();
+      return WalkResult::advance();
+    });
+    if (result.wasInterrupted())
+      return signalPassFailure();
+  }
+}
+
+hir::FuncOp SpecializeFuncsPass::specialize(hir::FuncOp originalFunc,
+                                            mir::SpecializedFuncAttr spec) {
+  // Make sure we have enough regions.
+  if (originalFunc.getNumRegions() < 2) {
+    emitBug(originalFunc.getLoc())
+        << "function does not have a second region to specialize";
+    return {};
+  }
+
+  // Make sure we have the correct number of arguments.
+  auto argsExpected = spec.getArgs().size() + spec.getConsts().size();
+  auto argsActual = originalFunc.getRegion(1).getNumArguments();
+  if (argsExpected != argsActual) {
+    emitBug(originalFunc.getLoc())
+        << "function has " << argsActual
+        << " arguments, but specialization expects " << argsExpected << " ("
+        << spec.getArgs().size() << " args and " << spec.getConsts().size()
+        << " consts)";
+    return {};
+  }
+
+  // Create a clone of the function with the first region dropped.
+  OpBuilder builder(originalFunc);
+  builder.setInsertionPointAfter(originalFunc);
+  auto func = builder.create<hir::FuncOp>(originalFunc.getLoc(),
+                                          originalFunc.getSymNameAttr(),
+                                          originalFunc.getNumRegions() - 1);
+  for (unsigned regionIdx = 0; regionIdx < func.getNumRegions(); ++regionIdx) {
+    auto &srcRegion = originalFunc.getRegion(regionIdx + 1);
+    auto &dstRegion = func.getRegion(regionIdx);
+    IRMapping mapper;
+    srcRegion.cloneInto(&dstRegion, mapper);
+  }
+
+  // Apply argument type specialization.
+  auto &block = func.getRegion(0).front();
+  builder.setInsertionPointToStart(&block);
+  unsigned argIdx = 0;
+  for (auto type : spec.getArgs()) {
+    auto arg = block.getArgument(argIdx++);
+    auto castOp = UnrealizedConversionCastOp::create(
+        builder, arg.getLoc(), arg.getType(), ValueRange{arg});
+    arg.setType(type);
+    arg.replaceAllUsesExcept(castOp.getResult(0), castOp);
+  }
+
+  // Materialize constant arguments.
+  auto firstConstIdx = argIdx;
+  for (auto attr : spec.getConsts()) {
+    auto arg = block.getArgument(argIdx++);
+    auto constOp =
+        mir::ConstantOp::create(builder, arg.getLoc(), cast<TypedAttr>(attr));
+    if (constOp.getType() == arg.getType()) {
+      arg.replaceAllUsesWith(constOp);
+      continue;
+    }
+    auto castOp = UnrealizedConversionCastOp::create(
+        builder, arg.getLoc(), arg.getType(), ValueRange{constOp});
+    arg.replaceAllUsesExcept(castOp.getResult(0), castOp);
+  }
+  block.eraseArguments(firstConstIdx, argIdx - firstConstIdx);
+
+  return func;
+}
---
+++ test-mlir/Transforms/specialize-funcs.mlir
@@ -0,0 +1,54 @@
+// RUN: silicon-opt --specialize-funcs %s | FileCheck %s
+
+func.func private @use_value(%arg0: !hir.value)
+
+// CHECK: hir.constant_func [[SIMPLE_SPEC:@Simple_.+]]
+// CHECK: hir.constant_func [[SIMPLE_SPEC]]
+mir.constant #mir.specialized_func<@Simple, [!mir.int], [], [#mir.int<42>]>
+mir.constant #mir.specialized_func<@Simple, [!mir.int], [], [#mir.int<42>]>
+
+// CHECK-LABEL: hir.func @Simple
+// CHECK: hir.func [[SIMPLE_SPEC]]
+hir.func @Simple {
+  mir.return
+}, {
+// CHECK-NEXT: ^bb0(%arg0: !mir.int):
+^bb0(%arg0: !hir.value, %arg1: !hir.value):
+  // CHECK-NEXT: [[ARG0:%.+]] = builtin.unrealized_conversion_cast %arg0 : !mir.int to !hir.value
+  // CHECK-NEXT: [[TMP:%.+]] = mir.constant #mir.int<42>
+  // CHECK-NEXT: [[ARG1:%.+]] = builtin.unrealized_conversion_cast [[TMP]] : !mir.int to !hir.value
+  // CHECK-NEXT: func.call @use_value([[ARG0]]) : (!hir.value) -> ()
+  func.call @use_value(%arg0) : (!hir.value) -> ()
+  // CHECK-NEXT: func.call @use_value([[ARG1]]) : (!hir.value) -> ()
+  func.call @use_value(%arg1) : (!hir.value) -> ()
+  // CHECK-NEXT: mir.return
+  mir.return
+}
+
+// CHECK: hir.constant_func [[NESTED_OUTER_SPEC:@NestedOuter_.+]]
+// CHECK: hir.constant_func [[NESTED_OUTER_SPEC]]
+mir.constant #mir.specialized_func<@NestedOuter, [], [], [#mir.specialized_func<@NestedInner, [], [], [#mir.int<1337>]>]>
+mir.constant #mir.specialized_func<@NestedOuter, [], [], [#mir.specialized_func<@NestedInner, [], [], [#mir.int<1337>]>]>
+
+// CHECK-LABEL: hir.func @NestedOuter
+// CHECK: hir.func [[NESTED_OUTER_SPEC]]
+hir.func @NestedOuter {
+  mir.return
+}, {
+^bb0(%arg0: !hir.func):
+  // CHECK-NEXT: hir.constant_func [[NESTED_INNER_SPEC:@NestedInner_.+]]
+  mir.return
+}
+
+// CHECK-LABEL: hir.func @NestedInner
+// CHECK: hir.func [[NESTED_INNER_SPEC]]
+hir.func @NestedInner {
+  mir.return
+}, {
+^bb0(%arg0: !hir.value):
+  // CHECK-NEXT: [[TMP:%.+]] = mir.constant #mir.int<1337>
+  // CHECK-NEXT: [[ARG0:%.+]] = builtin.unrealized_conversion_cast [[TMP]] : !mir.int to !hir.value
+  // CHECK-NEXT: func.call @use_value([[ARG0]]) : (!hir.value) -> ()
+  func.call @use_value(%arg0) : (!hir.value) -> ()
+  mir.return
+}
