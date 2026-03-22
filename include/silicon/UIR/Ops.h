4529762838c1e4604f0d946ea2432e001f96c19b
Fabian Schuiki <fabian@schuiki.ch>
2026-03-21 21:43:41 -0700

    [UIR] Formatting; NFC

--- include/silicon/UIR/Ops.h
+++ include/silicon/UIR/Ops.h
@@ -11,11 +11,11 @@
 #include "silicon/HIR/Types.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/SymbolTable.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
-#include "mlir/IR/OpDefinition.h"
 
 // Pull in the generated op definitions.
 #define GET_OP_CLASSES
